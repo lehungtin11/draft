3903e89a5dcceb1530fcf999c0f88f6b4c4399c3
lehungtin11 <hungtin640@gmail,com>
2023-11-15 10:29:53 +0700

    Webservice

---
+++ webservice/ClientApp/build/precache-manifest.738352b5fdc4680aff62c39e64af7ae8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c65ab7d2cc6585a4c9332c0f5c322412",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fda8e53656040641674a",
+    "url": "/static/css/2.55ef3354.chunk.css"
+  },
+  {
+    "revision": "e4ee00baf8a80c3b5c3a",
+    "url": "/static/css/main.1043b8f6.chunk.css"
+  },
+  {
+    "revision": "fda8e53656040641674a",
+    "url": "/static/js/2.73e6fb97.chunk.js"
+  },
+  {
+    "revision": "b716ccffba7eb1947b24230578e0f784",
+    "url": "/static/js/2.73e6fb97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ee00baf8a80c3b5c3a",
+    "url": "/static/js/main.3517899d.chunk.js"
+  },
+  {
+    "revision": "e56f0a0650dc33ec7a9c",
+    "url": "/static/js/runtime-main.67740278.js"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "f4a8a1253a08d963e06f03c2e90aacfb",
+    "url": "/static/media/logo-3cx.f4a8a125.PNG"
+  }
+]);
